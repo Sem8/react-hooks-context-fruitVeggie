880228a3d03ec68af18f82dadfef6be02f0805b0
Sem8 <mssemmi8@gmail.com>
2019-06-02 04:11:20 -0400

    finishing up spreading data across components using useContext and createContext

--- fruit-veggie/src/App.js
+++ fruit-veggie/src/App.js
@@ -1,14 +1,18 @@
 import React, { useContext } from "react";
-import { FruitContext } from "./Store";
+import { FruitContext, VegetableContext } from "./Store";
 import Vegetable from './Vegetable';
 
+
 const App = () => {
-  const [fruit, setFruit] = useContext(FruitContext);
+  const [fruit] = useContext(FruitContext);
+  const [vegetable] = useContext(VegetableContext);
   return (
     <div>
       <h1>React Hooks in Context</h1>
       <h2>Today's fruit is {fruit}</h2>
       <Vegetable />
+      <h3>Veggies</h3>
+      {vegetable}
     </div>
   );
 };
--- fruit-veggie/src/Vegetable.js
+++ fruit-veggie/src/Vegetable.js
@@ -2,10 +2,11 @@ import React, { useContext } from 'react';
 import { VegetableContext } from './Store';
 
 const Vegetable = () => {
-    const [vegetable, setVegetable] = useContext(VegetableContext);
+    // const [vegetable, setVegetable] = useContext(VegetableContext);
+    const [, setVegetable] = useContext(VegetableContext);
     return (
         <>
-        <div>{vegetable}</div>
+        {/* <div>{vegetable}</div> */}
         <button onClick={() => setVegetable('Brocolli')}>Set Vege to Broc</button>
         </>
     )
